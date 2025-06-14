ca8826cc427566ff538639b08e8441b27a53b0f3
seiumiyata <161181844+seiumiyata@users.noreply.github.com>
2025-06-14 23:14:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,9 +4,14 @@ const urlsToCache = [
   '/index.html',
   '/style.css',
   '/app.js',
-  'https://cdn.jsdelivr.net/npm/@marp-team/marp-core/lib/browser.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+  'https://cdn.jsdelivr.net/npm/@marp-team/marp-core@latest/lib/browser/index.min.js',
+  'https://cdn.jsdelivr.net/npm/pptxgenjs@latest/dist/pptxgen.bundle.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/codemirror.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/mode/markdown/markdown.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/codemirror.min.css'
 ];
+
 
 self.addEventListener('install', event => {
   event.waitUntil(
